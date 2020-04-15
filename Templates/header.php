e6932cfa9f385b785c500ed804c55b202993045f
unknown <you@example.com>
2020-04-15 13:44:46 +0700

    first commit

---
+++ Templates/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pemrograman Visual Kasir</title>
+</head>
+<body>
+    <header>
+        <h1>Pemrograman Visual Kasir</h1>
+        <h2>Apilikasi Sederhana Kasir</h2>
+    </header>
+    
